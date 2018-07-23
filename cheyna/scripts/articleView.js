ce07ea76f7ce36c1cee05a9b1a4f6cb3ab7be281
Cheyna Humphries <humphries.c92@gmail.com>
2018-07-23 14:12:57 -0700

    fixed up what I could on the lab, still having some issues with the new article showing correctly

--- cheyna/scripts/articleView.js
+++ cheyna/scripts/articleView.js
@@ -77,11 +77,11 @@ articleView.setTeasers = () => {
 // COMMENT: Where is this function called? Why?
 // This is called in the new.html page because this js is used to make the new html functional.
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show();
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
+  $('#export-field').hide();
   $('#article-json').on('focus', function(){
     this.select();
   });
@@ -106,17 +106,17 @@ articleView.create = () => {
     publishedOn: $('#article-published:checked').length ? new Date() : null
   });
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-  // $('#articles').append(Handlebars.compile(article));
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').append(article.toHtml());
 
-  $('#articles', Handlebars.compile($('#article-template').text(article)))
-  
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each(function() {
-
+  // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
   });
-  
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+
+  // DONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(`${JSON.stringify(article)},`);
 };
 
 // COMMENT: Where is this function called? Why?
